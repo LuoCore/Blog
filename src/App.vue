81cb9a58358f08efa674cd58a6e518d59fa50a4c
LuoCore <luocore@hotmail.com>
2021-07-13 21:29:40 +0800

    修改看有没有其效果

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <template>
   <Signin  />
- <Navbar />> 
-  <HeroSection />
-  <DemosSection  />
-  <CartzillaPagesSection  />
-  <CartzillHandheldToolbar  />
+  <Navbar />
   <FooterPage  />
-  <ToolbarForHandheldDevices />
   <BackToTopButton />
 </template>
 
@@ -15,12 +10,7 @@ import { defineComponent } from 'vue'
 
 import  Signin  from './components/SigninModal.vue'
 import  Navbar  from './components/Navbar.vue'
-import  HeroSection  from './components/HeroSection.vue'
-import  DemosSection  from './components/DemosSection.vue'
-import  CartzillaPagesSection  from './components/CartzillaPagesSection.vue'
-import  CartzillHandheldToolbar  from './components/CartzillHandheldToolbar.vue'
 import  FooterPage  from './components/FooterPage.vue'
-import  ToolbarForHandheldDevices  from './components/ToolbarForHandheldDevices.vue'
 import  BackToTopButton  from './components/BackToTopButton.vue'
 
 export default defineComponent({
@@ -28,12 +18,7 @@ export default defineComponent({
   components: {
     Signin,
     Navbar,
-    HeroSection,
-    DemosSection,
-    CartzillaPagesSection,
-    CartzillHandheldToolbar,
     FooterPage,
-    ToolbarForHandheldDevices,
     BackToTopButton
   }
 })
