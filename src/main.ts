ec66de9af7abaef5aaeb2ec31114dab1f8f936c8
LuoCore <luocore@hotmail.com>
2021-07-20 13:40:33 +0800

    211

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import App from './App.vue'
 import router from "./router/index";
 
 
-
+router.beforeEach((to, from, next) => {
+    // 让页面回到顶部
+    document.documentElement.scrollTop = 0
+    // 调用 next()，一定要调用 next 方法，否则钩子就不会被销毁
+   next()
+})
 createApp(App).use(router).mount('#app')
 
 
