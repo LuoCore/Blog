1e2037bd9ac6e6d8a8efc09e0582e789a1f04ef2
LuoCore <LuoCore@hotmail.com>
2021-12-24 16:53:48 +0800

    123

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from "./router/index";
 
 
+
 router.beforeEach((to, from, next) => {
     // 让页面回到顶部
     document.documentElement.scrollTop = 0
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   build: {
     outDir: "docs", //指定打包输出路径
-  }
+  },
+  
 })
+
